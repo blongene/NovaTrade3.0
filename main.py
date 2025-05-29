1a1df75eec905b19fc86734126ca394be8880467
blongene <blongene@gmail.com>
2025-05-29 16:16:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from telegram_webhook import telegram_app
 from presale_scorer import run_presale_scorer
 from nova_watchdog import run_watchdog
 from rotation_signal_engine import scan_rotation_candidates
-from roi_followups import scan_roi_tracking
-from sync_token_vault import sync_token_vault
+from roi_milestone_alert import scan_roi_tracking
+from token_vault_sync import sync_token_vault
 
 app = telegram_app
 
