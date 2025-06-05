9931a1a34fee59abcb620a73d28a0f672f93cd29
blongene <blongene@gmail.com>
2025-06-05 18:36:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from staking_yield_tracker import run_staking_yield_tracker
 from rotation_stats_sync import run_rotation_stats_sync
 from rotation_feedback_engine import run_rotation_feedback_engine
 from performance_dashboard import run_performance_dashboard
-from rebalance_scanner import run_rotation_stats_sync
+from rebalance_scanner import run_rebalance_scanner
 from rotation_log_updater import run_rotation_log_updater
 
 import time
