d69f12ce380fe453c51d65ebc96b22572f8060ae
blongene <blongene@gmail.com>
2025-08-22 22:27:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -312,11 +312,12 @@ else:
 
 import os
 from telegram_webhook import telegram_app, set_telegram_webhook
-from nova_watchdog import start_watchdog
+from nova_watchdog import run_watchdog
 
 if __name__ == "__main__":
     print("📡 Orion Cloud Boot Sequence Initiated")
     set_telegram_webhook()
-    start_watchdog()
-    telegram_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    run_watchdog()
+    port = int(os.getenv("PORT", "10000"))
+    telegram_app.run(host="0.0.0.0", port=port)
 
