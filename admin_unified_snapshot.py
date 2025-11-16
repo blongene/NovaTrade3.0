1954fc8814cb2fc43409db93af79344cc348c0f3
blongene <blongene@gmail.com>
2025-11-16 18:13:35 -0500

    Create admin_unified_snapshot.py

---
+++ admin_unified_snapshot.py
@@ -0,0 +1,7 @@
+# admin_unified_snapshot.py
+# One-off helper to build the Unified_Snapshot sheet on demand.
+
+from unified_snapshot import run_unified_snapshot
+
+if __name__ == "__main__":
+    run_unified_snapshot()
